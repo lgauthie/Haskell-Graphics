c0918a3f24c1a64a88c54d8b0c50f572dcf73563
lgauthie <lgauthie@uvic.ca>
2012-11-11 00:22:08 -0800

    Simplify display callback using forM.

--- src/Display.hs
+++ src/Display.hs
@@ -2,6 +2,7 @@ module Display (idle, display) where
  
 import qualified Graphics.Rendering.OpenGL as GL
 import qualified Graphics.UI.GLUT as GLUT
+import Control.Monad (forM)
 
 ($=!) :: (GL.HasSetter s) => s a -> a -> IO ()
 ($=!) = (GL.$=!)
@@ -16,18 +17,22 @@ display angle = do
     a <- GLUT.get angle
     GL.rotate a $ GL.Vector3 1 1 (1 :: GL.GLfloat)
     GL.scale 0.5 0.5 (0.5 :: GL.GLfloat)
+
+    forM (points 17) (\(x,y,z) ->
+            GL.preservingMatrix $ renderBoxAndWire x y z
+        )
+    GLUT.swapBuffers
+
+renderBoxAndWire x y z = do
     scube <- GL.defineNewList GL.Compile $ do
         GLUT.renderObject GLUT.Solid $ GLUT.Cube (0.1)
     lcube <- GLUT.defineNewList GLUT.Compile $ do
         GLUT.renderObject GLUT.Wireframe $ GLUT.Cube (0.1)
-    mapM_ (\(x,y,z) -> GL.preservingMatrix $ do
-        GL.color $ GL.Color3 ((x+1.0)/2.0) ((y+1.0)/2.0) ((z+1.0)/2.0)
-        GL.translate $ GL.Vector3 x y z
-        GL.callList scube
-        GL.color $ GL.Color3 0.0 0.0 (0.0 :: GL.GLfloat)
-        GL.callList lcube
-        ) $ points 17
-    GLUT.swapBuffers
+    GL.translate $ GL.Vector3 x y z
+    GL.color $ GL.Color3 ((x+1.0)/2.0) ((y+1.0)/2.0) ((z+1.0)/2.0)
+    GL.callList scube
+    GL.color $ GL.Color3 0.0 0.0 (0.0 :: GL.GLfloat)
+    GL.callList lcube
  
 idle angle = do
     a <- GLUT.get angle
