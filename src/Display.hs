38f059237356f21c930183c15c49dd7f7bce7459
lgauthie <lgauthie@uvic.ca>
2012-11-10 23:42:35 -0800

    Add qualified imports to make it clear what comes from where.

--- src/Display.hs
+++ src/Display.hs
@@ -1,34 +1,35 @@
 module Display (idle, display) where
  
-import Graphics.Rendering.OpenGL
-import Graphics.UI.GLUT
- 
-import Cube
+import qualified Graphics.Rendering.OpenGL as GL
+import qualified Graphics.UI.GLUT as GLUT
 
-points :: Int -> [(GLfloat,GLfloat,GLfloat)]
+($=!) :: (GL.HasSetter s) => s a -> a -> IO ()
+($=!) = (GL.$=!)
+
+points :: Int -> [(GL.GLfloat,GL.GLfloat,GL.GLfloat)]
 points n' = let n = fromIntegral n' in
     map (\k -> let t = 2*pi*k/n in (sin(t),0,cos(t)))  [1..n]
  
 display angle = do 
-    clear [DepthBuffer, ColorBuffer]
-    loadIdentity
-    a <- get angle
-    rotate a $ Vector3 1 1 (1::GLfloat)
-    scale 0.5 0.5 (0.5 :: GLfloat)
-    scube <- defineNewList Compile $ do
-        renderObject Solid $ Cube (0.1)
-    lcube <- defineNewList Compile $ do
-        renderObject Wireframe $ Cube (0.1)
-    mapM_ (\(x,y,z) -> preservingMatrix $ do
-        color $ Color3 ((x+1.0)/2.0) ((y+1.0)/2.0) ((z+1.0)/2.0)
-        translate $ Vector3 x y z
-        callList scube
-        color $ Color3 0.0 0.0 (0.0::GLfloat)
-        callList lcube
+    GL.clear [GL.DepthBuffer, GL.ColorBuffer]
+    GL.loadIdentity
+    a <- GLUT.get angle
+    GL.rotate a $ GL.Vector3 1 1 (1 :: GL.GLfloat)
+    GL.scale 0.5 0.5 (0.5 :: GL.GLfloat)
+    scube <- GL.defineNewList GL.Compile $ do
+        GLUT.renderObject GLUT.Solid $ GLUT.Cube (0.1)
+    lcube <- GLUT.defineNewList GLUT.Compile $ do
+        GLUT.renderObject GLUT.Wireframe $ GLUT.Cube (0.1)
+    mapM_ (\(x,y,z) -> GL.preservingMatrix $ do
+        GL.color $ GL.Color3 ((x+1.0)/2.0) ((y+1.0)/2.0) ((z+1.0)/2.0)
+        GL.translate $ GL.Vector3 x y z
+        GL.callList scube
+        GL.color $ GL.Color3 0.0 0.0 (0.0 :: GL.GLfloat)
+        GL.callList lcube
         ) $ points 17
-    swapBuffers
+    GLUT.swapBuffers
  
 idle angle = do
-    a <- get angle
+    a <- GLUT.get angle
     angle $=! (a + 0.04)
-    postRedisplay Nothing
+    GLUT.postRedisplay Nothing
--- src/HelloWorld.hs
+++ src/HelloWorld.hs
@@ -1,45 +1,53 @@
-import Graphics.Rendering.OpenGL
-import Graphics.UI.GLUT
+import qualified Graphics.Rendering.OpenGL as GL
+import qualified Graphics.UI.GLUT as GLUT
 import Data.IORef
- 
-import Bindings
 
-lightDiffuse :: Color4 GLfloat
-lightDiffuse = Color4 1.0 1.0 0.0 1.0
+import Bindings (reshape, keyboardMouse)
+import Display (idle, display)
 
-lightPosition :: Vertex4 GLfloat
-lightPosition = Vertex4 1.0 1.0 1.0 1.0
+($=) :: (GL.HasSetter s) => s a -> a -> IO ()
+($=) = (GL.$=)
+
+lightDiffuse :: GLUT.Color4 GL.GLfloat
+lightDiffuse = GLUT.Color4 1.0 1.0 0.0 1.0
+
+lightPosition :: GLUT.Vertex4 GL.GLfloat
+lightPosition = GLUT.Vertex4 1.0 1.0 1.0 1.0
  
+main :: IO ()
 main = do
-    (progname,_) <- getArgsAndInitialize
+    (progname,_) <- GLUT.getArgsAndInitialize
     initOpenGL
-    reshapeCallback $= Just reshape
-    keyboardMouseCallback $= Just keyboardMouse
-    angle <- newIORef 0.0
-    displayCallback $= (display angle)
-    idleCallback $= Just (idle angle)
-    mainLoop
+    angle <- newIORef 0.0 -- A nice "Mutable" IO Monad
 
-initOpenGL = let light0 = Light 0 in do
-    initialDisplayMode $= [RGBMode,WithDepthBuffer,DoubleBuffered]
+    GLUT.reshapeCallback $= Just reshape
+    GLUT.keyboardMouseCallback $= Just keyboardMouse -- TODO: Something fun with this
+    GLUT.displayCallback $= (display angle)
+    GLUT.idleCallback $= Just (idle angle)
 
-    initialWindowSize $= Size 1024 786 
-    initialWindowPosition $= Position 0 0
-    createWindow "Hello World"
+    GLUT.mainLoop
 
-    depthFunc $= Just Lequal
-    matrixMode $= Projection
-    depthMask $= Enabled
-    viewport $= (Position 0 0 , Size 1024 786)
-    perspective 90 ((fromIntegral 1024)/(fromIntegral 786)) 0.01 1000
-    lookAt (Vertex3 0.0 0.0 1.0) (Vertex3 0.0 0.0 0.0) (Vector3 0.0 1.0 0.0)
-    matrixMode $= Modelview 0
+initOpenGL :: IO ()
+initOpenGL = let light0 = GL.Light 0 in do
+    GLUT.initialDisplayMode $= [GLUT.RGBMode,GLUT.WithDepthBuffer,GLUT.DoubleBuffered]
 
-    clearColor $= Color4 0.0 0.0 0.0 1.0
-    blendFunc $= (SrcAlpha, OneMinusSrcAlpha)
-    colorMaterial $= Just (FrontAndBack, AmbientAndDiffuse)
-    texture Texture3D $= Enabled
-    diffuse light0 $= lightDiffuse
-    position light0 $= lightPosition
-    light light0 $= Enabled
-    lighting $= Enabled
+    GLUT.initialWindowSize $= GL.Size 1024 786
+    GLUT.initialWindowPosition $= GL.Position 0 0
+    GLUT.createWindow "Hello World"
+
+    GL.depthFunc $= Just GL.Lequal
+    GL.matrixMode $= GL.Projection
+    GL.depthMask $= GL.Enabled
+    GL.viewport $= (GL.Position 0 0 , GL.Size 1024 786)
+    GL.perspective 90 ((fromIntegral 1024)/(fromIntegral 786)) 0.01 1000
+    GL.lookAt (GL.Vertex3 0.0 0.0 1.0) (GL.Vertex3 0.0 0.0 0.0) (GL.Vector3 0.0 1.0 0.0)
+    GL.matrixMode $= GL.Modelview 0
+
+    GL.clearColor $= GL.Color4 0.0 0.0 0.0 1.0
+    GL.blendFunc $= (GL.SrcAlpha, GL.OneMinusSrcAlpha)
+    GL.colorMaterial $= Just (GL.FrontAndBack, GL.AmbientAndDiffuse)
+    GL.texture GLUT.Texture3D $= GL.Enabled
+    GL.diffuse light0 $= lightDiffuse
+    GL.position light0 $= lightPosition
+    GL.light light0 $= GL.Enabled
+    GL.lighting $= GL.Enabled
