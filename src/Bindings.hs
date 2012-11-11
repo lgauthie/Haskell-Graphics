36c10ac3caf34c19340b4983338679e19ba279e9
lgauthie <lgauthie@uvic.ca>
2012-11-10 23:51:05 -0800

    Remove unused pointless import.

--- src/Bindings.hs
+++ src/Bindings.hs
@@ -1,9 +1,7 @@
-module Bindings (idle,display,reshape,keyboardMouse) where 
+module Bindings (reshape,keyboardMouse) where
 
 import Graphics.Rendering.OpenGL
 import Graphics.UI.GLUT
- 
-import Display
  
 reshape s@(Size w h) = do 
     viewport $= (Position 0 0, s)
